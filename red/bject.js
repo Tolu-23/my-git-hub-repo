c7408be5d1167081eda6c3b5bb00c9e0d68214d7
tolu21 <tolulopebamisile@gmail.com>
2022-07-03 15:31:04 +0100

    new

--- red/bject.js
+++ red/bject.js
@@ -1,28 +1,28 @@
-// const object = {};
-// object.$1 = 'faat';
-// object['1'] = faat;
+const object = {};
+object.$1 = 'faat';
+object['1'] = faat;
 
-// function longestString(){
-//     longest= '';
-//     for(var i=0; i<arguments.length; i++){
-//         if (arguments[i].length >longest.length) {
-//             longest = arguments[i];   
-//         }
-//     }
-//     return longest;
-// }
-// const sub = (x, y)=> y.every(eke => x.includes(eke));
+function longestString(){
+    longest= '';
+    for(var i=0; i<arguments.length; i++){
+        if (arguments[i].length >longest.length) {
+            longest = arguments[i];   
+        }
+    }
+    return longest;
+}
+const sub = (x, y)=> y.every(eke => x.includes(eke));
 
-// const sb = [1,2,3,4,5,6,7,8];
-// sb.every((wea, index, sb)=>{
-//     sb[index] -= 1;
-//     console.log(`[${sb}][${index}] -> ${wea}`);
-//     return wea<8
-// })
+const sb = [1,2,3,4,5,6,7,8];
+sb.every((wea, index, sb)=>{
+    sb[index] -= 1;
+    console.log(`[${sb}][${index}] -> ${wea}`);
+    return wea<8
+})
 
-const b = [1,2,3,4,5,6,7,8];
-b.every((we, index, b)=>{
-    b.pop();
+const ber = [1,2,3,4,5,6,7,8];
+ber.every((we, index, b)=>{
+    ber.pop();
     console.log(`[${b}][${index}] -> ${we}`);
     return we<8
 })
@@ -125,7 +125,122 @@ console.log(appleStore.next())      // { value: 7, done: false }
 console.log(appleStore.next())      // { value: 5, done: false }
 console.log(appleStore.next())      // { value: undefined, done: true }
 
-// const dar = Date.prototype.getDate(1999,06,02);
-// console.log(dar)
+const dar = Date.prototype.getDate(1999,06,02);
+console.log(dar)
 
 console.log(Date.now())
+const aq = [-10, 22, 333, 42];
+const bc = [-11, 5, 22, 41, 42];
+let vas = [...aq, ...bc]
+let sae = new Set(vas)
+let arrs =Array.from(sae);
+let srt = arrs.sort()
+console.log(srt);
+
+
+function merge(left, right) {
+  let arr = []
+  while (left.length && right.length) {
+      if (left[0] < right[0]) {
+          arr.push(left.shift())  
+      } else {
+          arr.push(right.shift()) 
+      }
+  }
+  
+  return [ ...arr, ...left, ...right ]
+}
+function mergeSort(array) {
+  const half = array.length / 2
+  
+  if(array.length < 2){
+    return array 
+  }
+  
+  const left = array.splice(0, half)
+  return merge(mergeSort(left),mergeSort(array))
+}
+console.log(mergeSort(arrs));
+
+
+
+let arr = [false,'2',1];
+const search =  false;
+const search_index = arr.indexOf(search);
+
+if (search_index >= 0){
+arr.splice(search_index, 1)
+}
+console.log(`${arr}`)
+
+const d = [-10,-11,-3,1,-4,2,7]
+const e = -3
+let car = [];
+for(let i = 0; i<d.length; i++){
+  if(d[i] > e){
+    c.push(d[i]); 
+  }
+}
+  console.log(c)
+
+var a = [1,2,3,5,6]
+var b = 1
+b<6? a.unshift(0) : a.unshift(b)
+console.log(a)
+
+console.log(Array.from(new Array(10-2+1).keys()))// // 'a': map.get(1)
+
+var c = 'refgty';
+var v = 'cdefgh'
+
+const h = [...c, ...v];
+let x = [...v].reverse();
+console.log(x)
+console.log(h.join(''))
+console.log(h)
+
+const t = 'tpircs';
+function specialString(str){
+return str.replace(/[^\w]/g, '')
+}
+console.log(specialString(t));
+
+function myFunction(a, b){
+const q = a;
+const t = b;
+function specialString(str){
+return str.replace(/[^\w]/g, '')
+}
+let r = specialString(t);
+let s = specialString(q);
+let v = s.charAt(0).toUpperCase() + s.slice(1);
+console.log(v);
+let n = [...r].reverse();
+const f = [...v, ...n];
+const h = f.join('');
+return h
+}
+console.log(myFunction('java', 'tpi%rcs'))
+console.log(myFunction('c%ountry', 'edis'))
+console.log(myFunction('down', 'nw%ot'))
+
+const daq = [1,2,3];
+const caz = 2;
+let dh = [];
+for(let i = 0; i<daq.length; i++){
+  if(daq[i] != caz){
+    dh.push(daq[i]); 
+  }
+}
+console.log(dh);
+
+const nextPrimeNumber = (num) => {
+    for(let i = 2; i < num ; i++) {
+        if(num % i === 0) {
+            return nextPrimeNumber(num + 1)
+        }
+
+    }
+    return num;
+};
+console.log(nextPrimeNumber(2000));
